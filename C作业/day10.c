351774b8497da3e002f96bf43eb16cfefbef5ecb
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-04-17 22:08:59 +0800

    Update day10.c

--- C作业/day10.c
+++ C作业/day10.c
@@ -7,9 +7,13 @@ int count_one_bits(unsigned int value)
 // 返回 1的位数 
 } 
 
+
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+
+//第一种
+#if 0
 int main()
 {
 	int count = 0;
@@ -23,8 +27,57 @@ int main()
 			count++;
 			i = i / 2;
 		}
-		else  i = i / 2;
+		else  i = i / 2;        //i=i>>1;
 	}
+	printf("count=%d\n", count);
+	system("pause");
+	return 0;
+}
+#endif 
+
+
+//第二种
+#if 0
+int  count_one_bits(unsigned int n)     //unsigened有无都可以
+{
+	int count = 0;
+	while (n != 0)
+	{
+		n = n&(n - 1);
+		count++;
+	}
+	return count;
+}
+int main()
+{
+	int n;
+	printf("请输入一个数:");
+	scanf("%d", &n);
+	int count = count_one_bits(n);
+	printf("count=%d\n", count);
+	system("pause");
+	return 0;
+}
+#endif
+
+
+//第三种
+int  count_one_bits(unsigned int n)
+{
+	int count = 0;
+	for (int i = 0; i < 32; i++)
+	{
+		if (((n >> i) & 1) == 1)
+			count++;
+	}
+	return count;
+}
+int main()
+{
+	int n;
+	printf("请输入一个数:");
+	scanf("%d", &n);
+	int count = count_one_bits(n);
 	printf("count=%d\n", count);
 	system("pause");
 	return 0;
