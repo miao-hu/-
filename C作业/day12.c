8de4536bc057c1c700273e471c526cc12af91c6f
miao-hu <48383488+miao-hu@users.noreply.github.com>
2019-04-22 11:42:16 +0800

    Update day12.c

--- C作业/day12.c
+++ C作业/day12.c
@@ -15,6 +15,7 @@ unsigned int reverse_bit(unsigned int value);
 #include<stdlib.h>
 #include<math.h>
 
+#if 0
 int InvertArr(int *arr)
 {
 	int arr1[32] = { 0 };
@@ -59,8 +60,30 @@ int main()
 		a /= 2;
 	}
 	//Printf(arr);
-	
 	unsigned int ret=InvertArr(arr);
+	printf("%u\n", ret);
+	system("pause");
+	return 0;
+}
+#endif
+
+
+//第二种
+unsigned int  InvertArr(unsigned int num)
+{
+	unsigned int sum = 0;
+	for (int i = 0; i < 32; i++)
+	{
+		sum+=((num>> i) & 1) * pow(2, 31 - i);
+	}
+	return sum;
+}
+int main()
+{
+	unsigned int num;
+	printf("Please enter number:");
+	scanf("%d", &num);
+	unsigned int ret = InvertArr(num);
 	printf("%u\n", ret);
 	system("pause");
 	return 0;
@@ -73,8 +96,9 @@ int main()
 #include<stdio.h>
 #include<stdlib.h>
 
+//第一种
 #if 0
-float Average(float a, float b)
+float Average1(float a, float b)
 {
 	return a+(b-a)/2;
 }
@@ -83,36 +107,51 @@ int main()
 	float a, b;
 	printf("请输入两个数:");
 	scanf("%f %f", &a, &b);
-	float ret = Average(a, b);
+	float ret = Average1(a, b);
 	printf("ret=%f\n", ret);
 	system("pause");
 	return 0;
 }
 #endif
 
-int Average(int a, int b)
-{
-	return (a&b) + (a^b) >> 1;
+//第二种
+/*
+a和b相同的部分：&运算
+a和b不同的部分：^运算
+相同的部分乘以2，加上不同的部分为两者的和
+除以2 相当于>>1
+*/
+
+int Average2(int a, int b)
+{                                  //   +的优先级大于>>
+	return (a&b) + ((a^b) >> 1);   // 注意操作符的优先级(a^b) >> 1必须带括号
+	//return (a&b) + (a^b) / 2;
 }
 int main()
 {
 	int a, b;
 	printf("请输入两个数:");
 	scanf("%d %d", &a, &b);
-	int ret = Average(a, b);
-	printf("ret=%d\n", ret);
+	int average= Average2(a, b);
+	printf("average=%d\n", average);
 	system("pause");
 	return 0;
 }
+
+
+
 
 3.编程实现： 
 一组数据中只有一个数字出现了一次。其他所有数字都是成对出现的。 
 请找出这个数字。（使用位运算） 
 
+
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
 //一个数异或0为它本身，一个数异或它本身为0        异或相同为0，不同为1
+
+//(1)
 #if 0
 int main()
 {
@@ -124,6 +163,7 @@ int main()
 }
 #endif
 
+//(2)
 int Fun(int *arr, int len)
 {
 	int i, temp = 0;
@@ -159,29 +199,56 @@ i am a student
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void Invertarray(char *p)
+
+void Reverse(char *left,char *right)  //逆置字符串left-->right
 {
-	int len = strlen(p);
-	int temp = *p;
-	*p = *(p + len - 1);
-	*(p + len - 1) = '\0';
-	if (strlen(p + 1) > 1)
+	char temp;
+	while(left<=right)
 	{
-		Invertarray(p+1);
+	temp=*left;
+	*left=*right;
+	*right=temp;
+	left++;
+	right--;
 	}
-	*(p + len - 1) = temp;
+}
+int MyStrlen(char *str)    //求字符串长度
+{
+	int len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return len;
+}
+void ReverseSentence(char *str)
+{
+	//1.先逆置整个字符串
+	char *left = str;
+	char *right = str + MyStrlen(str) - 1;
+	char *p = str;
+	Reverse(left,right); 
+	//2.再逆置每个单词
+	while (*p != '\0')
+	{
+		char *start = p;
+		while (*p != ' '&&*p != '\0')
+		{
+			p++;
+		}
+		Reverse(start, p - 1);
+		if (*p == ' ')
+		{
+			p++;
+		}
+	}
 }
 int main()
 {
-	char s[] = "student a am i";
-	char temp;
-	printf("%s\n", s);
-	Invertarray(s);
-	Invertarray(s+7);
-	temp = s[2];
-	s[2] = s[3];
-	s[3] = temp;
-	printf("%s\n", s);
+	char str[] = "student a am i";
+	ReverseSentence(str);
+	printf("%s\n", str);
 	system("pause");
 	return 0;
 }
