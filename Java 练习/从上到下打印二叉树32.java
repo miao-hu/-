d0372d68c14b196676692925fa8d710cf076c416
miao-hu <48383488+miao-hu@users.noreply.github.com>
2020-07-31 09:00:42 +0800

    Create 从上到下打印二叉树32.java

---
+++ Java 练习/从上到下打印二叉树32.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class 从上到下打印二叉树32 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            List<Integer> list=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                list.add(node.val);
+                if(node.left!=null){
+                    q.offer(node.left);
+                }
+                if(node.right!=null){
+                    q.offer(node.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
